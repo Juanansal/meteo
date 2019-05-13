c1c2a5e13b0ca2c94d11ebe9579b4c5939d8f782
Juanansal <Juanansal@gmail.com>
2019-05-13 12:07:34 +0200

    crear home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './global/components/cabecera/cabecera.component';
+import { PieComponent } from './global/components/pie/pie.component';
+import { HomeComponent } from './global/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
